e67caeddbef173e63950dc9cf0986b494d68f9bb
AmirSaidov <printerp99@gmail.com>
2026-05-07 03:56:32 +0600

    admin-ru

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -108,9 +109,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+LANGUAGES = [
+    ("ru", "Русский"),
+    ("en", "English"),
+]
+
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Nexus QR — Админка"
+admin.site.site_title = "Nexus QR — Админка"
+admin.site.index_title = "Панель управления"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('rooms.urls')), 
